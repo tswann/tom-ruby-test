435936b16d6befabab8fee2b4f04c2340d41e887
Thomas Swann <ts.swann@gmail.com>
2013-11-19 22:04:34 +0000

    Layouts, datamapper, postgres

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bundle exec ruby web.rb -p $PORT
+web: bundle exec ruby recall.rb -p $PORT
--- recall.rb
+++ recall.rb
@@ -1,2 +1,21 @@
 require 'sinatra'
 require 'datamapper'
+
+DataMapper.setup(:default, '')
+
+class Note
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :complete, Boolean, :required => true, default => false
+  property :created_at, DateTime
+  property :updated_at, DateTime
+end
+
+DataMapper.finalize.auto_upgrade!
+
+get '/' do
+  @notes = Note.all :order => :id.desc
+  @title = 'All Notes'
+  erb :home
+end
