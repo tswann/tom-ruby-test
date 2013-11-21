d201097d5be18fa9461fa14f15946118bc9dea0a
Tom Swann <vagrant@precise32.(none)>
2013-11-21 21:41:50 +0000

    fixed symbol

--- recall.rb
+++ recall.rb
@@ -7,7 +7,7 @@ class Note
   include DataMapper::Resource
   property :id, Serial
   property :content, Text, :required => true
-  property :complete, Boolean, :required => true, default => false
+  property :complete, Boolean, :required => true, :default => false
   property :created_at, DateTime
   property :updated_at, DateTime
 end
